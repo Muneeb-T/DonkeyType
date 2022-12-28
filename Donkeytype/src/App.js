9eca976161adceca55898f88311becae016e9ef6
Muneeb-T <muneebmnb17@gmail.com>
2022-12-28 12:10:00 +0530

    initial commit
    This is the first commit of this project. The basic environment for this project has set. Installed react, configured webpack and babel and configured tailwind css

---
+++ Donkeytype/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div className='bg-gray-700 h-screen flex items-center justify-center'>
+            <h1 className='text-3xl text-gray-300 font-bold'>
+                Welcome to DonkeyType
+            </h1>
+        </div>
+    );
+}
